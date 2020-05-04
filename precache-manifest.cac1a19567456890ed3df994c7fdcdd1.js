b53dd1e4102fab4b5334952734105c76e483af2f
John Fairclough <johnmfairclough@gmail.com>
2020-05-04 15:46:31 +0100

    Updates

---
+++ precache-manifest.cac1a19567456890ed3df994c7fdcdd1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcea69b014cba3044dabbdb37b5e4995",
+    "url": "/job-search/index.html"
+  },
+  {
+    "revision": "92d4319f5cd1583bb008",
+    "url": "/job-search/static/css/main.f5055963.chunk.css"
+  },
+  {
+    "revision": "ae69b49d9b761cfb4a72",
+    "url": "/job-search/static/js/2.ba250bb1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/job-search/static/js/2.ba250bb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92d4319f5cd1583bb008",
+    "url": "/job-search/static/js/main.7bcae503.chunk.js"
+  },
+  {
+    "revision": "86010f23f3c0812eae0e",
+    "url": "/job-search/static/js/runtime-main.214efbc8.js"
+  }
+]);
